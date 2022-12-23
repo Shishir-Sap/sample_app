29607ebe21301745b782ddcc43d280f1092f8823
shishir-sap <shishirsapkota@msn.com>
2022-12-23 11:49:06 +0000

    sample_app

---
+++ tempdir/sample_app.py
@@ -0,0 +1,20 @@
+# Add to this file for the microweb app lab
+from flask import Flask
+from flask import request
+from flask import render_template
+import datetime
+
+microweb_app = Flask(__name__)
+
+
+@microweb_app.route("/foto")
+def document():
+    return render_template("my_foto.html")
+
+@microweb_app.route("/")
+def main():
+    return render_template("index.html", datetime_now = datetime.datetime.now())
+
+if __name__ == "__main__":
+    microweb_app.run(host="0.0.0.0", port=8008)
+    #
